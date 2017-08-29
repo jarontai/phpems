e9367c4df6a4c11507e7ac6ed6e7eb435af24c81
Jaron Tai <jaroncn@gmail.com>
2017-08-29 15:31:17 +0800

    update db config

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -15,10 +15,10 @@ define('WP','http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']));
 else
 define('WP','http://'.$_SERVER['SERVER_NAME'].'/');
 
-define('DB','666');//MYSQL数据库名
+define('DB','phpems');//MYSQL数据库名
 define('DH','localhost');//MYSQL主机名，不用改
 define('DU','root');//MYSQL数据库用户名
-define('DP','');//MYSQL数据库用户密码
+define('DP','root');//MYSQL数据库用户密码
 define('DTH','x2_');//系统表前缀，不用改
 
 define('WXAPPID','wx0f2a632abf082e60');
